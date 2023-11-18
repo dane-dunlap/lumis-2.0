7b341bcaa473e77474493c3af559e2133b58e926
Dane Dunlap <danecooperdunlap@gmail.com>
2023-11-18 08:42:58 +0400

    made changes to env variables

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,10 @@ import AuthForm from './auth-form'
 
 export default function Home() {
   return (
-    <div className="row">
-        <div className="flex items-center justify-center mt-56">
-          <p className="text-9xl">Lumis ✨</p>
+        <div className="flex items-center justify-center h-screen">
+          <p className="text-8xl">Welcome to Lumis ✨</p>
         </div>
     
-    </div>
   )
 }
 
